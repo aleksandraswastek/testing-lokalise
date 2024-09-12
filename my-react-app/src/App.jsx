ef8745bb2c8606d54375b0dbed1a5c0299bd78bc
Aleksandra swastek <aleksandra.swastek@jit.team>
2024-09-12 15:27:36 +0200

    test:wq1

---
+++ my-react-app/src/App.jsx
@@ -0,0 +1,18 @@
+import { Suspense } from "react";
+import { useTranslation } from "react-i18next";
+
+function App() {
+  const { t } = useTranslation();
+  return (
+    <div>
+      <h1>{t("main.header")}</h1>
+    </div>
+  );
+}
+export default function WrappedApp() {
+  return (
+    <Suspense fallback="...loading">
+      <App />
+    </Suspense>
+  );
+}
